02f8fd6d1c007085d232e3b5a1bf3729086a2451
Roger <rogeliomer_2@hotmail.com>
2017-03-13 13:12:38 -0600

    Adding timestamp

---
+++ playground/time.js
@@ -0,0 +1,9 @@
+const moment = require('moment');
+
+// var date = moment();
+// date.add(100, 'year').subtract(9, 'months');
+// console.log(date.format('MMM Do, YYYY'));
+
+var date = moment();
+date.subtract(1, 'hour')
+console.log(date.format('h:mm a'));
